53687bd187e61f324b787b0c560c08ccda8f63b2
Aashish-Shah-dev <aashish4shah@gmail.com>
2024-07-07 10:39:35 +0545

    question10.py

--- question10.py
+++ question10.py
@@ -1,7 +1,10 @@
-n = list((int,input("Enter the list of number")))
-max_value = n[0]
-for number in n:
-    if number > max_value :
-       max_value = number 
-       
-print("The max value in the list is :",max_value)
+def find_maximum(numbers):
+    max_value = numbers[0]
+    for number in numbers:
+        if number > max_value:
+            max_value = number
+    return max_value
+
+numbers = [int(x) for x in input("Enter numbers separated by spaces: ").split()]
+max_value = find_maximum(numbers)
+print(f"The maximum value in the list is: {max_value}")
